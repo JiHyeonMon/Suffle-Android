a79db39b5524560b790d1efe28658be68a2449c9
JiHyeonMon <jhk8806@naver.com>
2020-08-11 03:21:37 +0900

    [HOME] 이미지 수정 + Home 레이아웃

--- app/src/main/java/com/example/suffle/ui/Home/HomeFragment.kt
+++ app/src/main/java/com/example/suffle/ui/Home/HomeFragment.kt
@@ -5,9 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.suffle.R
+import com.example.suffle.data.PlaceData
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
+
+    val datas = mutableListOf<PlaceData>()
+    lateinit var placeAdapter: PlaceAdapter
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,6 +23,71 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        placeAdapter = PlaceAdapter(view.context,
+            object : PlaceViewHolder.onClickListener {
+                override fun onClickItem(position: Int) {
+                    Toast.makeText(context, position.toString(), Toast.LENGTH_SHORT).show()
+                }
+            })
+
+        frag_home_rv_place.adapter = placeAdapter
+        frag_home_rv_place.setLayoutManager(LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false));
+
+        loadDatas()
+
+    }
+
+    private fun loadDatas(){
+
+
+        datas.apply {
+            add(
+                PlaceData(
+                    txt_place = "라라브레드",
+                    img_bookmark = false,
+                    txt_distance = "1.5km",
+                    txt_thumbUp = "김주은외 4,345",
+                    txt_thumbDown = "55"
+                )
+            )
+            add(
+                PlaceData(
+                    txt_place = "라라브레드",
+                    img_bookmark = false,
+                    txt_distance = "1.5km",
+                    txt_thumbUp = "김주은외 4,345",
+                    txt_thumbDown = "55"
+                )
+            )
+            add(
+                PlaceData(
+                    txt_place = "라라브레드",
+                    img_bookmark = false,
+                    txt_distance = "1.5km",
+                    txt_thumbUp = "김주은외 4,345",
+                    txt_thumbDown = "55"
+                )
+            )
+            add(
+                PlaceData(
+                    txt_place = "라라브레드",
+                    img_bookmark = false,
+                    txt_distance = "1.5km",
+                    txt_thumbUp = "김주은외 4,345",
+                    txt_thumbDown = "55"
+                )
+            )
+        }
+
+        placeAdapter.datas = datas
+        placeAdapter.notifyDataSetChanged()
+
     }
 
 }
